6cb23d175893d6fa5c14ea0210e727de3c3d8dc3
siddharth goyal <113788213+siddharthgoyal00@users.noreply.github.com>
2025-08-06 10:28:09 +0530

    docker

---
+++ docker/index.js
@@ -0,0 +1,23 @@
+const { Client } = require('pg');
+
+const connectionString = 'postgresql://postgres:mysecretpassword@localhost:5432/postgres';
+
+const client = new Client({
+    connectionString: connectionString
+});
+
+client.connect(err => {
+    if (err) {
+        console.error('connection error', err.stack);
+    } else {
+        console.log('connected to the database');
+    }
+});
+
+client.query('SELECT NOW()', (err, res) => {
+    if (err) {
+        console.error(err);
+    } else {
+        console.log(res.rows[0]);
+    }
+});
